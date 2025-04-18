f2217d2227a675f30e181448176774dfc9d13eaa
Aryan M <maheshwari.aryan24@gmail.com>
2025-04-17 22:04:46 -0500

    cors update for testing

--- scripts/main.py
+++ scripts/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://interviewai-bay.vercel.app", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
